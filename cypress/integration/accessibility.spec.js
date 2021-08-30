f755dfd9714acff90e5b751a0f1f8543120e0ada
Kacper Klarzyński <k.klarzynski@livechatinc.com>
2021-08-30 21:07:06 +0200

    add a simple test using the keyboard navigation

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -9,4 +9,25 @@ describe("dummy form", () => {
     cy.checkA11y();
   });
 
+  const assertFocusedInputLabel = (expectedLabel) => {
+    return cy.focused().then(($el) => {
+      cy.get(`label[for="${$el.attr("id")}"]`).should(
+        "have.text",
+        expectedLabel
+      );
+    });
+  };
+
+  it("should allow submitting with just keyboard", () => {
+    cy.findByText("Dummy form").should("be.visible").realClick();
+    cy.realPress("Tab");
+    assertFocusedInputLabel("Name:").realType("Dummy");
+    cy.realPress("Tab");
+    assertFocusedInputLabel("Last Name:").realType("User");
+    cy.realPress("Tab");
+    assertFocusedInputLabel("Books").realPress("Space");
+    cy.realPress("Enter");
+    cy.findByText("Thanks for submitting the form!").should("be.visible");
+  });
+
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,3 +2,4 @@
 import './commands'
 import 'cypress-axe'
 import '@testing-library/cypress/add-commands';
+import "cypress-real-events/support";
